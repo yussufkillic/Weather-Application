7dd8912fd4e734847241bff8f6d6bcf1a83258d8
yussufkillic <134396677+yussufkillic@users.noreply.github.com>
2024-07-07 17:41:33 +0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,70 @@
+import './App.css'
+import axios from 'axios'
+import { useState, useEffect} from 'react'
+
+function App() {
+  const [weatherData,setWeatherData] = useState(null);
+  const [location, setLocation] = useState('');
+
+  console.log(import.meta.env.VITE_WEATHER_API);
+
+  useEffect(()=> {
+    const fetchData = async () => 
+    {
+        try {
+          const response = await axios.get(`http://api.weatherapi.com/v1/forecast.json?key=${import.meta.env.VITE_WEATHER_API}&q=${location}&days=4&aqi=yes&alerts=yes`)
+          setWeatherData(response.data);
+          console.log(response);
+        } 
+        
+        catch (error) {
+          console.log(error);
+        }
+    };
+    if(location)
+    {
+      fetchData();
+    }
+
+  }, [location])
+
+  const handleLocationChange = (event) => 
+  {
+    setLocation(event.target.value)
+  }
+
+  return (
+  <>
+  <div className='app-container'>
+    <h1 className='app-title'>Weather Application</h1>
+      <div className='input-container'>
+          <input className='location-input'
+          type='text'
+          placeholder='Şehir giriniz'
+          value={location}
+          onChange={handleLocationChange}
+          />
+      </div>
+  </div>
+
+  {weatherData && (
+    <div className='weather-container'>
+      {weatherData.forecast.forecastday.map( (day) => (
+
+      <div className='day-container' key={day.date}>
+        <h2 className='date'>{day.date}</h2>
+        <img className='weather-icon' src={day.day.condition.icon} alt={day.day.condition.text}/>
+        <p className='temperature'>{day.day.avgtemp_c} C</p>
+        <p className='temperature'>{day.day.condition.text}</p>
+      </div>
+
+      ))}
+    </div>
+  )}
+
+  
+  </>      
+  )
+}
+
+export default App
